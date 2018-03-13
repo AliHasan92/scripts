edc93073f4befdf6b557918a916104a4630f8aef
Ali Hasan <admin@ialihasan.com>
2018-03-13 07:11:46 +0000

    Set up build environment

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# This scripts set up build environment
+# for Ubuntu 16.04 and up
+# Using scripts from
+# https://raw.githubusercontent.com/nathanchance/Android-Tools/master/Guides/Building_AOSP.txt
+
+sudo apt-get install git-core
+git clone https://github.com/akhilnarang/scripts
+cd scripts
+bash setup/ubuntu1604linuxmint18.sh
