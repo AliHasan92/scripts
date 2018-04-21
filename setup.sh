5fad60bc5822e89e6c02b5eeab3c73b7fd85c77a
Ali Hasan <36104227+AliHasan96@users.noreply.github.com>
2018-04-21 19:23:58 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # This script can be used by anyone but make sure to set your real indentity, here I am setting up mine.
-# This scripts set up build environment
+# This scripts can be used set up build environment for ROM building
 # for Ubuntu 16.04 and up
 # Using scripts from
 # https://raw.githubusercontent.com/nathanchance/Android-Tools/master/Guides/Building_AOSP.txt
+# Thanks to nathanchance for his scripts, I just made it auto for my ease.
 
 cd ~
 sudo apt-get install git-core
